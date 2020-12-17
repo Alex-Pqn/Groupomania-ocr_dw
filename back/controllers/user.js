59ebd93908c33e81a32d1cb9178d7a83253ce0b3
Alexandre P <@paquienalexandre0@gmail.com>
2020-12-17 21:46:35 +0100

    fix double db call register & many fixs

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -15,47 +15,40 @@ exports.register = (req, res, next) => {
       // insert in db the new account
       db.query(
         `INSERT INTO Accounts (firstname, lastname, email, hashed_password, newsletters) VALUES ('${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${hashed_password}', ${req.body.newsletters});`, 
-        (err) => {
+        (err, result) => {
+        let userDb = result
+        
         // error handler
         if(err) {
-          // account with this e-mail already exists
-          if (err.code === 'ER_DUP_ENTRY') {
+          // duplicated entry code (account with this e-mail already exist)
+          if (err.errno === 1062) {
             return res.status(401).json({ sub_error: "Il semblerait qu'un compte possédant cet e-mail existe déjà.", err })
+          // other errors
           }else {
             return res.status(401).json({ sub_error: "La création de votre compte a échoué, veuillez réessayer dans quelques instants.", err })
           }
         }
-        // search the new created account (for get user id to return in front)
-        db.query(
-          `SELECT * FROM Accounts WHERE email LIKE '${req.body.email}'`, 
-          (err, result) => {
-          let userDb = result[0]
-          
-          // error handler
-          if(err) {
-            return res.status(401).json({ sub_error: "La création de votre compte a échoué, veuillez réessayer dans quelques instants.", err })
-          }
-          // account created with success
-          return res.status(200).json({ 
-            message: `Nous te souhaitons la bienvenue ${req.body.firstname} !`,
-            userId: userDb.id,
-            token: jwt.sign(
-              { userId: userDb.id },
-              process.env.KEY_TOKEN_AUTH,
-              { expiresIn: "24h" }
-            )
-          })
+        // account created
+        return res.status(200).json({ 
+          message: `Nous te souhaitons la bienvenue ${req.body.firstname} !`,
+          userId: userDb.insertId,
+          token: jwt.sign(
+            { userId: userDb.insertId },
+            process.env.KEY_TOKEN_AUTH,
+            { expiresIn: "24h" }
+          )
         })
       })
     })
-    .catch((err) => res.status(500).json({ err }))
+    .catch((err) => res.status(500).json({ sub_error: "La création de votre compte a échoué, veuillez réessayer dans quelques instants.", err }))
 }
-  
+ 
 //login
 exports.login = (req, res, next) => {
   // search a row in db with this e-mail
-  db.query(`SELECT * FROM Accounts WHERE email LIKE '${req.body.email}'`, 
-  (err, result) => {
+  db.query(
+    `SELECT * FROM Accounts WHERE email LIKE '${req.body.email}'`, 
+    (err, result) => {
     let userDb = result[0]
     
     // error handler
@@ -63,7 +56,7 @@ exports.login = (req, res, next) => {
       return res.status(401).json({ sub_error: "La tentative d'authentification a échoué, veuillez réessayer dans quelques instants.", err })
     }
     // no one account finded
-    if(result.length === 0) {
+    if(userDb.length === 0) {
       return res.status(401).json({ sub_error: "L'e-mail et le mot de passe entré sont incorrects.", err })
     // account finded 
     }else{
