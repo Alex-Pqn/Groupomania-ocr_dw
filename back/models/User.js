5425d1ef1f5bff0427286b12b6a93135511759e1
Alexandre P <@paquienalexandre0@gmail.com>
2020-12-19 20:12:56 +0100

    regexp pattern user schema

--- back/models/User.js
+++ back/models/User.js
@@ -3,20 +3,24 @@ const Joi = require('joi');
 const userSchema = Joi.object({
     firstname: Joi.string()
         .min(3)
-        .max(30),
+        .max(30)
+        .pattern(new RegExp("/[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/")),
         
     lastname: Joi.string()
         .min(3)
-        .max(30),
+        .max(30)
+        .pattern(new RegExp("/[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/")),
     
     email: Joi.string().email()
         .min(5)
         .max(55)
-        .required(),
+        .required()
+        .pattern(new RegExp("/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g")),
         
     password: Joi.string()
         .min(8)
-        .max(50),
+        .max(50)
+        .pattern(new RegExp("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm")),
         
     newsletters: Joi.boolean(),
     
