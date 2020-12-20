b6694e284dda179abb90ca83af62f9df6f1b4264
Alexandre P <@paquienalexandre0@gmail.com>
2020-12-21 00:22:51 +0100

    login fix: password & email don't matchs

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -14,38 +14,38 @@ exports.pageAuth = (req, res, next) => {
 
 //register
 exports.register = (req, res, next) => {
-  // hash password with bcrypt
-  bcrypt.hash(req.body.password, bcryptSaltRounds)
-    .then(function(hashed_password) {
-      // insert in db the new account
-      db.query(
-        `INSERT INTO Accounts (firstname, lastname, email, hashed_password, newsletters) VALUES ('${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${hashed_password}', ${req.body.newsletters});`, 
-        (err, result) => {
-        let userDb = result
-        
-        // error handler
-        if(err) {
-          // duplicated entry code (account with this e-mail already exist)
-          if (err.errno === 1062) {
-            return res.status(401).json({ sub_error: "Il semblerait qu'un compte possédant cet e-mail existe déjà.", err })
-          // other errors
-          }else {
-            return res.status(401).json({ sub_error: "La création de votre compte a échoué, veuillez réessayer dans quelques instants.", err })
-          }
+// hash password with bcrypt
+bcrypt.hash(req.body.password, bcryptSaltRounds)
+  .then(function(hashed_password) {
+    // insert in db the new account
+    db.query(
+      `INSERT INTO Accounts (firstname, lastname, email, hashed_password, newsletters) VALUES ('${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${hashed_password}', ${req.body.newsletters});`, 
+      (err, result) => {
+      let userDb = result
+      
+      // error handler
+      if(err) {
+        // duplicated entry code (account with this e-mail already exist)
+        if (err.errno === 1062) {
+          return res.status(401).json({ sub_error: "Il semblerait qu'un compte possédant cet e-mail est déjà existant.", err })
+        // other
+        }else {
+          return res.status(401).json({ sub_error: "La création de votre compte a échoué, veuillez réessayer dans quelques instants.", err })
         }
-        // account created
-        return res.status(200).json({ 
-          message: `Nous te souhaitons la bienvenue ${req.body.firstname} !`,
-          userId: userDb.insertId,
-          token: jwt.sign(
-            { userId: userDb.insertId },
-            process.env.KEY_TOKEN_AUTH,
-            { expiresIn: "24h" }
-          )
-        })
+      }
+      // account created
+      return res.status(200).json({ 
+        message: `Nous te souhaitons la bienvenue ${req.body.firstname} !`,
+        userId: userDb.insertId,
+        token: jwt.sign(
+          { userId: userDb.insertId },
+          process.env.KEY_TOKEN_AUTH,
+          { expiresIn: "24h" }
+        )
       })
     })
-    .catch((err) => res.status(500).json({ sub_error: "La création de votre compte a échoué, veuillez réessayer dans quelques instants.", err }))
+  })
+  .catch((err) => res.status(500).json({ sub_error: "La création de votre compte a échoué, veuillez réessayer dans quelques instants.", err }))
 }
  
 //login
@@ -61,7 +61,7 @@ exports.login = (req, res, next) => {
       return res.status(401).json({ sub_error: "La tentative d'authentification a échoué, veuillez réessayer dans quelques instants.", err })
     }
     // no one account finded
-    if(userDb.length === 0) {
+    if(result.length === 0) {
       return res.status(401).json({ sub_error: "L'e-mail et le mot de passe entré sont incorrects.", err })
     // account finded 
     }else{
@@ -87,7 +87,7 @@ exports.login = (req, res, next) => {
         .catch(err => res.status(500).json({ sub_error: "La tentative d'authentification a échoué, veuillez réessayer dans quelques instants.", err }))
       }
     }
-  )
+  ) 
 }
   
 exports.getParameters = (req, res, next) => {
