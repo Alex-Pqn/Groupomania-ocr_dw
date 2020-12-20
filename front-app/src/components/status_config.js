b1b8232c42e1ce4926bdac3c52ad4c10a5d47eb0
Alexandre P <@paquienalexandre0@gmail.com>
2020-12-21 00:24:57 +0100

    adding BAD_REQUEST status

--- front-app/src/components/status_config.js
+++ front-app/src/components/status_config.js
@@ -1,7 +1,8 @@
 module.exports.http = {
     OK: 200,
     UNAUTHORIZED: 401,
-    INTERNAL_SERVER_ERROR: 401
+    INTERNAL_SERVER_ERROR: 401,
+    BAD_REQUEST: 400
 }
 module.exports.readystate = {
     DONE: 4
