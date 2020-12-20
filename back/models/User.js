ed9161acc8771b2b4e2704c6d24f99e1dec1c320
Alexandre P <@paquienalexandre0@gmail.com>
2020-12-21 00:24:09 +0100

    pattern/regex fix

--- back/models/User.js
+++ back/models/User.js
@@ -1,26 +1,26 @@
 const Joi = require('joi');
 
-const userSchema = Joi.object({
+module.exports = Joi.object().keys({
     firstname: Joi.string()
         .min(3)
         .max(30)
-        .pattern(new RegExp("/[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/")),
+        .pattern(new RegExp(/[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/)),
         
     lastname: Joi.string()
         .min(3)
         .max(30)
-        .pattern(new RegExp("/[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/")),
+        .pattern(new RegExp(/[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/)),
     
     email: Joi.string().email()
         .min(5)
         .max(55)
         .required()
-        .pattern(new RegExp("/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g")),
+        .pattern(new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)),
         
     password: Joi.string()
         .min(8)
         .max(50)
-        .pattern(new RegExp("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm")),
+        .pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)),
         
     newsletters: Joi.boolean(),
     
@@ -29,5 +29,3 @@ const userSchema = Joi.object({
         
     pic_url: Joi.string()
 })
-
-module.exports = userSchema
