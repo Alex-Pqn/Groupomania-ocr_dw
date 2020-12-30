8cdf08b238e357c4f0c5fd1b236e8a790dcca9ba
Alexandre P <@paquienalexandre0@gmail.com>
2020-12-30 19:16:07 +0100

    lint fix + api & apiCallback functions import

--- front-app/src/components/forums/userCreateForum.vue
+++ front-app/src/components/forums/userCreateForum.vue
@@ -1,101 +1,155 @@
 <template>
-<!-- create forum -->
-<article class="create-forum">
-  <!-- content -->
-  <div class="create-forum__content">
-    <textarea maxlength="320" placeholder="Quoi de neuf aujourd'hui ?" id="create-forum_text"></textarea>
-    <label for="create-forum_upload-img">
-      <img src="@/assets/cloud-upload-alt-solid.svg" alt="">
-    </label>
-    <input @change="forumCreateImgChange($event)" id="create-forum_upload-img" type="file" accept="image/*">
-    <button v-on:click="forumCreateSend">
-      <img src="@/assets/paper-plane-solid.svg" alt="">
-    </button>
-  </div>
-  <!-- img output -->
-  <div id="create-forum_img" class="create-forum__img-output">
-    <img id="create-forum_img-output" src="" alt="">
-    <div>
-      <img v-on:click="forumCreateImgClose" src="@/assets/times-solid.svg" alt="">
+  <!-- create forum -->
+  <article class="create-forum">
+    <!-- content -->
+    <div class="create-forum__content">
+      <textarea
+        maxlength="320"
+        placeholder="Quoi de neuf aujourd'hui ?"
+        id="create-forum_text"
+      ></textarea>
+      <label for="create-forum_upload-img">
+        <img src="@/assets/cloud-upload-alt-solid.svg" alt="" />
+      </label>
+      <input
+        @change="forumCreateImgChange($event)"
+        id="create-forum_upload-img"
+        type="file"
+        accept="image/*"
+        name="image"
+      />
+      <button v-on:click="forumCreateSend">
+        <img src="@/assets/paper-plane-solid.svg" alt="" />
+      </button>
     </div>
-  </div>
-</article>
+    <!-- img output -->
+    <div id="create-forum_img" class="create-forum__img-output">
+      <img id="create-forum_img-output" src="" alt="" />
+      <div>
+        <img
+          v-on:click="forumCreateImgClose"
+          src="@/assets/times-solid.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  </article>
 </template>
 
 <script>
+import { api, apiCallback } from "@/utils/scripts";
+const status = require("../../utils/status_config");
+
 export default {
   name: "userCreateForum",
-  props: {
-
-  },
+  props: {},
   methods: {
-    forumCreateImgChange (event) {
-      let reader = new FileReader()
-      reader.onload = function () {
-        document.getElementById('create-forum_img-output').src = reader.result
-        document.getElementById('create-forum_img').style.display = "flex"
-      }
-      reader.readAsDataURL(event.target.files[0])
-      console.log(event.target.files[0])
+    forumCreateImgChange(event) {
+      let reader = new FileReader();
+      reader.onload = function() {
+        document.getElementById("create-forum_img-output").src = reader.result;
+        document.getElementById("create-forum_img").style.display = "flex";
+      };
+      reader.readAsDataURL(event.target.files[0]);
     },
-    forumCreateImgClose () {
-      document.getElementById('create-forum_img').style.display = "none"
-      document.getElementById('create-forum_upload-img').value = ""
-      document.getElementById('create-forum_img-output').src = ""
+    forumCreateImgClose() {
+      document.getElementById("create-forum_img").style.display = "none";
+      document.getElementById("create-forum_upload-img").value = "";
+      document.getElementById("create-forum_img-output").src = "";
     },
-    forumCreateSend () {
-      const status = require('../status_config')
+    forumCreateSend() {
+      // create formdata
+      let formData = new FormData();
       
-      // create formdata and attach image
-      let formData = new FormData()
-      let inputImage = document.querySelector('input[type=file]').files[0]
-      if(inputImage) {
-        formData.append('image', inputImage, inputImage.name)
+      // attach image
+      let inputImage = document.querySelector("input[type=file]").files[0];
+      if (inputImage) {
+        formData.append("image", inputImage, inputImage.name);
       }
+
+      let forumTextOutput = document.getElementById("create-forum_text").value;
+      let forum = {
+        text: forumTextOutput
+      };
+      formData.append("forum", JSON.stringify(forum));
+
+      // api request
+      api("api/forums/create", "POST", formData);
       
-      // auth
-      let cookie = document.cookie.split(';')
-      let cookieUserId
-      let cookieUserToken
-      if (cookie[0] === "" || cookie[1] === "" || cookie.length < 2) {
-        console.error("Le token d'authentification est incorrect ou a expiré.")
-      }else{
-        cookieUserId = cookie[0].replace('user_id=', '')
-        cookieUserToken = cookie[1].replace('auth_token=', '')
-        
-        let userParams = {
-          userId: cookieUserId,
-        }
-        
-        // xhr request
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://localhost:3000/api/forums/create', true);
-        xhr.setRequestHeader('Authorization', 'Bearer ' + cookieUserToken)
-        xhr.send(formData, JSON.stringify(userParams));
-          
-        xhr.onerror = () => {
-          // displaySubmitInfoError("Une erreur est survenue lors de la création de votre compte. Vérifiez l'état de vote connexion internet et réessayez.")
-        }
-        
-        xhr.onreadystatechange = function() {
-          let response = JSON.parse(this.response)
-          
-          // DONE & OK
-          if (this.readyState === status.readystate.DONE && this.status === status.http.OK) { 
-            console.log(response)
-            
+      // callback api request
+      setTimeout(() => {
+        let response = apiCallback().response
+        let readyState = apiCallback().readyState
+        let httpStatus = apiCallback().httpStatus
+
+        // DONE & OK
+        if (
+          readyState === status.readystate.DONE &&
+          httpStatus === status.http.OK
+        ) {
+          console.log(response);
           // ERRORS HANDLER
-          } else if (this.status === status.http.UNAUTHORIZED || this.status === status.http.INTERNAL_SERVER_ERROR || this.status === status.http.BAD_REQUEST) {
-            // displaySubmitInfoError(response.sub_error)
-            if(response.err) {
-              console.error(`HTTP Status: ${this.status} ; ReadyState Status: ${this.readyState} | Error: ${response.err.sqlMessage || response.err.message} ; Code: ${response.err.code} : ${response.err.errno} ; fatal?${response.err.fatal} ; SQLState: ${response.err.sqlState}`)
-            }else{
-              console.error(`HTTP Status: ${this.status} ; ReadyState Status: ${this.readyState}`)
-            }
+        } else if (
+          httpStatus === status.http.UNAUTHORIZED ||
+          httpStatus === status.http.INTERNAL_SERVER_ERROR ||
+          httpStatus === status.http.BAD_REQUEST
+        ) {
+        // displaySubmitInfoError(response.sub_error)
+        if (response.err) {
+          console.error(
+            `HTTP Status: ${httpStatus} ; ReadyState Status: ${readyState} | Error: ${response
+              .err.sqlMessage || response.err.message} ; Code: ${
+              response.err.code
+            } : ${response.err.errno} ; fatal?${
+              response.err.fatal
+            } ; SQLState: ${response.err.sqlState}`
+          );
+        } else {
+            console.error(
+              `HTTP Status: ${httpStatus} ; ReadyState Status: ${readyState}`
+            );
           }
         }
+      }, 50);
+    },
+    //eslint-disable-next-line
+    apiOnError (err) {
+      console.log('test error')
+      console.error(err);
+      // displaySubmitInfoError("Une erreur est survenue lors de la création de votre compte. Vérifiez l'état de vote connexion internet et réessayez.")
+    },
+    //eslint-disable-next-line
+    apiOnReadystatechange (response, readyState, httpStatus) {
+      console.log('test readystatechange', response, readyState, httpStatus)
+      // DONE & OK
+      if (
+        readyState === status.readystate.DONE &&
+        httpStatus === status.http.OK
+      ) {
+        console.log(response);
+        // ERRORS HANDLER
+      } else if (
+        httpStatus === status.http.UNAUTHORIZED ||
+        httpStatus === status.http.INTERNAL_SERVER_ERROR ||
+        httpStatus === status.http.BAD_REQUEST
+      ) {
+        // displaySubmitInfoError(response.sub_error)
+        if (response.err) {
+          console.error(
+            `HTTP Status: ${httpStatus} ; ReadyState Status: ${readyState} | Error: ${response
+              .err.sqlMessage || response.err.message} ; Code: ${
+              response.err.code
+            } : ${response.err.errno} ; fatal?${
+              response.err.fatal
+            } ; SQLState: ${response.err.sqlState}`
+          );
+        } else {
+          console.error(
+            `HTTP Status: ${httpStatus} ; ReadyState Status: ${readyState}`
+          );
+        }
       }
-    }
+    },
   }
 };
 </script>
@@ -118,15 +172,15 @@ export default {
     button {
       width: 10%;
       border: 1px solid $default_smooth-border;
-      background-color:rgb(235, 235, 235);
+      background-color: rgb(235, 235, 235);
       cursor: pointer;
       img {
         margin-top: 2px;
         width: 27px;
-        filter: invert(0.5) sepia(1) saturate(5) hue-rotate(175deg)
+        filter: invert(0.5) sepia(1) saturate(5) hue-rotate(175deg);
       }
       &:hover {
-        background-color:rgb(238, 238, 238);
+        background-color: rgb(238, 238, 238);
       }
     }
     label {
@@ -136,13 +190,13 @@ export default {
       justify-content: center;
       width: 8%;
       border: 1px solid $default_smooth-border;
-      background-color:rgb(231, 231, 231);
+      background-color: rgb(231, 231, 231);
       img {
         width: 30px;
-        filter: invert(0.3) sepia(5) saturate(10) hue-rotate(175deg)
+        filter: invert(0.3) sepia(5) saturate(10) hue-rotate(175deg);
       }
       &:hover {
-        background-color:rgb(224, 224, 224);
+        background-color: rgb(224, 224, 224);
       }
     }
     input {
@@ -164,11 +218,11 @@ export default {
         cursor: pointer;
         width: 10px;
         padding: 2.5px 4px;
-        border-radius: .2em;
+        border-radius: 0.2em;
         background-color: rgba(255, 255, 255, 0.8);
         &:hover {
-          transform: scale(.97);
-          opacity: .8;
+          transform: scale(0.97);
+          opacity: 0.8;
         }
       }
     }
