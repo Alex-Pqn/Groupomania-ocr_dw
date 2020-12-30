c45ba8082f1cb29126c4e147680283db78642cb4
Alexandre P <@paquienalexandre0@gmail.com>
2020-12-30 21:08:38 +0100

    data-validation middleware deleted

--- back/routes/user.js
+++ back/routes/user.js
@@ -3,13 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user');
-const validator = require('../middlewares/data-validation')
 const auth = require('../middlewares/auth');
 
 router.post('/page/auth', auth, userController.pageAuth),
 
-router.post('/login', validator, userController.login);
-router.post('/register', validator, userController.register);
+router.post('/login', userController.login);
+router.post('/register', userController.register);
 
 router.get('/parameters', auth, userController.getParameters);
 router.put('/parameters', auth, userController.updateParameters);
