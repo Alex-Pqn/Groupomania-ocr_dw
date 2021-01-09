4b6e73ec5b362aa618186cf2701ffb69dcecf1e9
Alexandre P <@paquienalexandre0@gmail.com>
2021-01-09 01:03:28 +0100

    Allow UserId Header

--- back/app.js
+++ back/app.js
@@ -15,7 +15,7 @@ const app = express();
 // headers, methods & authorizations
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, UserId, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 })
